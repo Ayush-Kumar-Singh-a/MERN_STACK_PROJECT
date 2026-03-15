f55d1e00d1098f13fe0ece3dd44b1a8c6c57463f
Ayush Kumar Singh <ayushsingh4720@gmai.com>
2026-03-15 18:47:45 +0530

    auth and db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,19 @@
 import express from "express";
 import path from "path";
-// import cors from "cors";
-
+import cors from "cors";
+import {serve} from "inngest/express";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import { inngest } from "./lib/inngest.js";
 const app = express();
 
 const __dirname = path.resolve();
 
+
+//middleWare
+app.use(express.json())
+//credential true meaning?? =>our server allows a browser to include cookies on request
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 // if(ENV.NODE_ENV !== "production"){
 //     app.use(
 //         cors({
@@ -16,6 +21,8 @@ const __dirname = path.resolve();
 //         })
 //     );
 // }
+
+app.use("/api/inngest", serve({client:inngest, functions}))
 
 app.get("/health", (req,res) =>{
     res.status(200).json({msg:"API is up and running"})
